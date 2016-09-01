7fb66e9900c820e258146b49cd2311218f3dbd75
Luca Bertinetto <luca.bertinetto@gmail.com>
2016-09-01 11:12:27 +0100

    experiment and get batch

---
+++ training/experiment.m
@@ -0,0 +1,259 @@
+% -------------------------------------------------------------------------------------------------
+function [net, stats] = experiment(imdb_video, varargin)
+% Experiment main function - creates a network and trains it on the dataset.
+% -------------------------------------------------------------------------------------------------
+    % Default parameters (set the experiment-specific ones in run_experiment)
+    opts.net.type = 'alexnet';
+    opts.net.conf = struct(); % Options depend on type of net.
+    opts.pretrain = false; % Location of model file set in env_paths.
+    opts.init.scale = 1;
+    opts.init.weightInitMethod = 'xavierimproved';
+    opts.init.initBias = 0.1;
+    opts.expDir = 'data'; % where to save the trained net
+    opts.numFetchThreads = 12; % used by vl_imreadjpg when reading dataset
+    opts.validation = 0.1; % fraction of imbd reserved to validation
+    opts.exemplarSize = 127; % exemplar (z) in the paper
+    opts.instanceSize = 255 - 2*8; % search region (x) in the paper
+    opts.loss.type = 'simple';
+    opts.loss.rPos = 16; % pixel with distance from center d > rPos are given a negative label
+    opts.loss.rNeg = 0; % if rNeg != 0 pixels rPos < d < rNeg are given a neutral label
+    opts.loss.labelWeight = 'balanced';
+    opts.numPairs =  5.32e4; % Number of example pairs per epoch, if empty, then equal to number of videos.
+    opts.randomSeed = 0;
+    opts.shuffleDataset = false; % do not shuffle the data to get reproducible experiments
+    opts.frameRange = 100; % range from the exemplar in which randomly pick the instance
+    opts.gpus = [];
+    opts.prefetch = false; % Both get_batch and cnn_train_dag depend on prefetch.
+    opts.train.numEpochs = 50;
+    r = exp((log(1e-5)-log(1e-2)) / (50-1)); % logspace(-2, -5, 50)
+    opts.train.learningRate = 1e-2 * r.^(0:opts.train.numEpochs-1);
+    opts.train.weightDecay = 5e-4;
+    opts.train.batchSize = 8; % we empirically observed that small batches work better
+    opts.train.numSubBatches = 1;
+    opts.train.profile = false;
+    % Data augmentation settings
+    opts.subMean = false;
+    opts.colorRange = 255;
+    opts.augment.translate = true;
+    opts.augment.maxTranslate = 4;
+    opts.augment.stretch = true;
+    opts.augment.maxStretch = 0.05;
+    opts.augment.color = true;
+    opts.augment.grayscale = 0; % likelihood of using grayscale pair
+    % Override default parameters if specified in run_experiment
+    opts = vl_argparse(opts, varargin);
+    % Get environment-specific default paths.
+    opts = env_paths_training(opts);
+    opts.train.gpus = opts.gpus;
+    opts.train.prefetch = opts.prefetch;
+% -------------------------------------------------------------------------------------------------
+    % Get ImageNet Video metadata
+    if isempty(imdb_video)
+        fprintf('loading imdb video...\n');
+        imdb_video = load(opts.imdbVideoPath);
+    end
+
+    % Load dataset statistics for data augmentation
+    [rgbMean_z, rgbVariance_z, rgbMean_x, rgbVariance_x] = load_stats(opts);
+    if opts.shuffleDataset
+        s = RandStream.create('mt19937ar', 'Seed', 'shuffle');
+        opts.randomSeed = s.Seed;
+    end
+
+    opts.train.expDir = opts.expDir;
+
+    rng(opts.randomSeed); % Re-seed before calling make_net.
+
+    % -------------------------------------------------------------------------------------------------
+    net = make_net(opts);
+    % -------------------------------------------------------------------------------------------------
+
+    [imdb_video, imdb] = choose_val_set(imdb_video, opts);
+
+    [resp_sz, resp_stride] = get_response_size(net, opts);
+    % We want an odd number so that we can center the target in the middle
+    assert(all(mod(resp_sz, 2) == 1), 'resp. size is not odd');
+
+    [net, derOutputs, label_inputs_fn] = setup_loss(net, resp_sz, resp_stride, opts.loss);
+
+    batch_fn = @(db, batch) get_batch(db, batch, ...
+                                        imdb_video, ...
+                                        opts.rootDataDir, ...
+                                        numel(opts.train.gpus) >= 1, ...
+                                        struct('exemplarSize', opts.exemplarSize, ...
+                                               'instanceSize', opts.instanceSize, ...
+                                               'frameRange', opts.frameRange, ...
+                                               'subMean', opts.subMean, ...
+                                               'colorRange', opts.colorRange, ...
+                                               'stats', struct('rgbMean_z', rgbMean_z, ...
+                                                               'rgbVariance_z', rgbVariance_z, ...
+                                                               'rgbMean_x', rgbMean_x, ...
+                                                               'rgbVariance_x', rgbVariance_x), ...
+                                               'augment', opts.augment, ...
+                                               'prefetch', opts.train.prefetch, ...
+                                               'numThreads', opts.numFetchThreads), ...
+                                        label_inputs_fn);
+
+    opts.train.derOutputs = derOutputs;
+    opts.train.randomSeed = opts.randomSeed;
+    % -------------------------------------------------------------------------------------------------
+    % Start training
+    [net, stats] = cnn_train_dag(net, imdb, batch_fn, opts.train);
+    % -------------------------------------------------------------------------------------------------
+end
+
+
+% -----------------------------------------------------------------------------------------------------
+function [rgbMean_z, rgbVariance_z, rgbMean_x, rgbVariance_x] = load_stats(opts)
+% Dataset image statistics for data augmentation
+% -----------------------------------------------------------------------------------------------------
+    stats = load(opts.imageStatsPath);
+    % Subtracted if opts.subMean is true
+    if ~isfield(stats, 'z')
+        rgbMean = reshape(stats.rgbMean, [1 1 3]);
+        rgbMean_z = rgbMean;
+        rgbMean_x = rgbMean;
+        [v,d] = eig(stats.rgbCovariance);
+        rgbVariance_z = 0.1*sqrt(d)*v';
+        rgbVariance_x = 0.1*sqrt(d)*v';
+    else
+        rgbMean_z = reshape(stats.z.rgbMean, [1 1 3]);
+        rgbMean_x = reshape(stats.x.rgbMean, [1 1 3]);
+        % Set data augmentation statistics, used if opts.augment.color is true
+        [v,d] = eig(stats.z.rgbCovariance);
+        rgbVariance_z = 0.1*sqrt(d)*v';
+        [v,d] = eig(stats.x.rgbCovariance);
+        rgbVariance_x = 0.1*sqrt(d)*v';
+    end
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function net = make_net(opts)
+% -------------------------------------------------------------------------------------------------
+    switch opts.net.type
+    case 'alexnet'
+        net = make_alexnet(opts);
+    case 'resnet'
+        net = make_resnet(opts);
+    end
+
+    % Save the net graph to disk.
+    inputs = {'exemplar', [opts.exemplarSize*[1 1] 3 opts.train.batchSize], ...
+              'instance', [opts.instanceSize*[1 1] 3 opts.train.batchSize]};
+    net_dot = net.print(inputs, 'Format', 'dot');
+    if ~exist(opts.expDir)
+        mkdir(opts.expDir);
+    end
+    f = fopen(fullfile(opts.expDir, 'arch.dot'), 'w');
+    fprintf(f, net_dot);
+    fclose(f);
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function [resp_sz, resp_stride] = get_response_size(net, opts)
+% -------------------------------------------------------------------------------------------------
+
+    sizes = net.getVarSizes({'exemplar', [opts.exemplarSize*[1 1] 3 256], ...
+                             'instance', [opts.instanceSize*[1 1] 3 256]});
+    resp_sz = sizes{net.getVarIndex('score')}(1:2);
+    rfs = net.getVarReceptiveFields('exemplar');
+    resp_stride = rfs(net.getVarIndex('score')).stride(1);
+    assert(all(rfs(net.getVarIndex('score')).stride == resp_stride));
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function [net, derOutputs, inputs_fn] = setup_loss(net, resp_sz, resp_stride, loss_opts)
+% Add layers to the network, specifies the losses to minimise, and
+% constructs a function that returns the inputs required by the loss layers.
+% -------------------------------------------------------------------------------------------------
+
+    % create label and weights for logistic loss
+    net.addLayer('objective', ...
+                 dagnn.Loss('loss', 'logistic'), ...
+                 {'score', 'eltwise_label'}, 'objective');
+    % adding weights to loss layer
+    [neg_eltwise, pos_eltwise, instanceWeight] = create_labels(...
+        resp_sz, loss_opts.labelWeight, ...
+        loss_opts.rPos/resp_stride, loss_opts.rNeg/resp_stride);
+    net.layers(end).block.opts = [...
+        net.layers(end).block.opts, ...
+        {'instanceWeights', instanceWeight}];
+
+    derOutputs = {'objective', 1};
+
+    inputs_fn = @(labels, obj_sz_z, obj_sz_x) get_label_inputs_simple(...
+        labels, obj_sz_z, obj_sz_x, pos_eltwise, neg_eltwise);
+
+
+    net.addLayer('errdisp', centerThrErr(), {'score', 'label'}, 'errdisp');
+    net.addLayer('errmax', MaxScoreErr(), {'score', 'label'}, 'errmax');
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function inputs = get_label_inputs_simple(labels, obj_sz_z, obj_sz_x, pos_eltwise, neg_eltwise)
+% GET_LABEL_INPUTS_SIMPME returns the network inputs that specify the labels.
+%
+% labels -- Label of +1 or -1 per image pair, size [1, n].
+% obj_sz_z -- Size of exemplar box, dims [2, n].
+% obj_sz_x -- Size of instance box, dims [2, n].
+% -------------------------------------------------------------------------------------------------
+
+    pos = (labels > 0);
+    neg = (labels < 0);
+
+    resp_sz = size(pos_eltwise);
+    eltwise_labels = zeros([resp_sz, 1, numel(labels)], 'single');
+    eltwise_labels(:,:,:,pos) = repmat(pos_eltwise, [1 1 1 sum(pos)]);
+    eltwise_labels(:,:,:,neg) = repmat(neg_eltwise, [1 1 1 sum(neg)]);
+    inputs = {'label', labels, ...
+              'eltwise_label', eltwise_labels};
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function [imdb_video, imdb] = choose_val_set(imdb_video, opts)
+% Designates some examples for validation.
+% It modifies imdb_video and constructs a dummy imdb.
+% -------------------------------------------------------------------------------------------------
+    TRAIN_SET = 1;
+    VAL_SET = 2;
+
+    % set opts.validation to validation and the rest to training.
+    size_dataset = numel(imdb_video.id);
+    size_validation = round(opts.validation * size_dataset);
+    size_training = size_dataset - size_validation;
+    imdb_video.set = uint8(zeros(1, size_dataset));
+    imdb_video.set(1:size_training) = TRAIN_SET;
+    imdb_video.set(size_training+1:end) = VAL_SET;
+
+    %% create imdb of indexes to imdb_video
+    % train and val from disjoint video sets
+    imdb = struct();
+    imdb.images = struct(); % we keep the images struct for consistency with cnn_train_dag (MatConvNet)
+    imdb.id = 1:opts.numPairs;
+    n_pairs_train = round(opts.numPairs * (1-opts.validation));
+    imdb.images.set = uint8(zeros(1, opts.numPairs)); % 1 -> train
+    imdb.images.set(1:n_pairs_train) = TRAIN_SET;
+    imdb.images.set(n_pairs_train+1:end) = VAL_SET;
+end
+
+
+% -------------------------------------------------------------------------------------------------
+function inputs = get_batch(db, batch, imdb_video, data_dir, use_gpu, sample_opts, label_inputs_fn)
+% Returns the inputs to the network.
+% -------------------------------------------------------------------------------------------------
+
+    [imout_z, imout_x, labels, sizes_z, sizes_x] = vid_get_random_batch(...
+        db, imdb_video, batch, data_dir, sample_opts);
+    if use_gpu
+        imout_z = gpuArray(imout_z);
+        imout_x = gpuArray(imout_x);
+    end
+    % Constructs full label inputs from output of vid_get_random_batch.
+    label_inputs = label_inputs_fn(labels, sizes_z, sizes_x);
+    inputs = [{'exemplar', imout_z, 'instance', imout_x}, label_inputs];
+end
---
+++ training/vid_get_random_batch.m
@@ -0,0 +1,264 @@
+% -----------------------------------------------------------------------------------------------------------------------
+function [imout_z, imout_x, labels, sizes_z, sizes_x] = vid_get_random_batch(imdb, imdb_video, batch, data_dir, varargin)
+% Return batch of pairs of input (z and x) and labels
+% (Sizes are returned as [height, width])
+% -----------------------------------------------------------------------------------------------------------------------
+    % Defines
+    RGB = 1;
+    GRAY = 2;
+    TRAIN_SET = 1;
+    VAL_SET = 2;
+    % Default parameters
+    opts.exemplarSize = [];
+    opts.instanceSize = [];
+    opts.frameRange = 50;
+    opts.subMean = false;
+    opts.colorRange = 255; % Adjust range from [0, 255] to [0, colorRange].
+    opts.stats.rgbMean_z = [];
+    opts.stats.rgbVariance_z = [];
+    opts.stats.rgbMean_x = [];
+    opts.stats.rgbVariance_x = [];
+    opts.augment.translate = false;
+    opts.augment.maxTranslate = []; % empty means no max
+    opts.augment.stretch = false;
+    opts.augment.maxStretch = 0.1;
+    opts.augment.color = false;
+    opts.augment.grayscale = 0;
+    opts.prefetch = false;
+    opts.numThreads = 12;
+    opts = vl_argparse(opts, varargin);
+% -----------------------------------------------------------------------------------------------------------------------
+    % Determine the set (e.g. train or val) of the batch.
+    batch_set = imdb.images.set(batch(1));
+    % Check all images in the batch are from the same set.
+    assert(all(batch_set == imdb.images.set(batch)));
+    batch_size = numel(batch);
+    % Decide rgb vs gray with given probabilities
+    pair_types_rgb = datasample(1:2, batch_size, 'Weights', [1-opts.augment.grayscale opts.augment.grayscale]);
+% -----------------------------------------------------------------------------------------------------------------------
+    % randomize a subset of the imdb of batch_size - decide set of videos to sample from
+    % NOTE: the purpose of imdb is to preserve compatibility with MatConvNet cnn_train_dag,
+    % it just defines size of an epoch and the type of batch (train/val).
+    ids_set = find(imdb_video.set==batch_set);
+    % sample one video for each pair
+    rnd_videos = datasample(ids_set, batch_size, 'Replace', false);
+    ids_pairs = rnd_videos(1:batch_size);
+    % Initialize pairs
+    % First load location for all pairs, all images will be loaded at once with vl_imreadjpeg
+    % objects contains metadata for all the pairs of the batch
+    objects = struct();
+    objects.set = batch_set * uint8(ones(1, batch_size));
+    objects.z = cell(1, batch_size);
+    objects.x = cell(1, batch_size);
+    % crops locations
+    crops_z_string = cell(1, batch_size);
+    crops_x_string = cell(1, batch_size);
+    labels = zeros(1, batch_size);
+    % final augmented crops
+    imout_z = zeros(opts.exemplarSize, opts.exemplarSize, 3, batch_size, 'single');
+    imout_x = zeros(opts.instanceSize, opts.instanceSize, 3, batch_size, 'single');
+
+    for i = 1:batch_size
+        % Crops from same videos, centered on the object
+        labels(i) = 1;
+        [objects.z{i}, objects.x{i}] = choose_pos_pair(imdb_video, ids_pairs(i), opts.frameRange);
+    end
+
+    % get absolute paths of crops locations
+    for i=1:batch_size
+        crops_z_string{i} = [strrep(fullfile(data_dir, objects.z{i}.frame_path), '.JPEG','') '.' num2str(objects.z{i}.track_id, '%02d') '.crop.z.jpg'];
+        crops_x_string{i} = [strrep(fullfile(data_dir, objects.x{i}.frame_path), '.JPEG','') '.' num2str(objects.x{i}.track_id, '%02d') '.crop.x.jpg'];
+    end
+    % prepare all the files to read
+    files = [crops_z_string crops_x_string];
+
+    % prefetch is used to load images in a separate thread
+    if opts.prefetch
+        error('to implement');
+    end
+
+    % read all the crops efficiently
+    crops = vl_imreadjpeg(files, 'numThreads', opts.numThreads);
+    crops_z = crops(1:batch_size);
+    crops_x = crops(batch_size+1 : end);
+    clear crops
+    % -----------------------------------------------------------------------------------------------------------------------
+    % Data augmentation
+    % Only augment during training.
+    if batch_set == TRAIN_SET
+        aug_opts = opts.augment;
+    else
+        aug_opts = struct('translate', false, ...
+                          'maxTranslate', 0, ...
+                          'stretch', false, ...
+                          'maxStretch', 0, ...
+                          'color', false);
+    end
+
+    aug_z = @(crop) acquire_augment(crop, opts.exemplarSize, opts.stats.rgbVariance_z, aug_opts);
+    aug_x = @(crop) acquire_augment(crop, opts.instanceSize, opts.stats.rgbVariance_x, aug_opts);
+
+    for i=1:batch_size
+
+        tmp_z = aug_z(crops_z{i});
+        tmp_x = aug_x(crops_x{i});
+
+        switch pair_types_rgb(i)
+            case RGB
+                imout_z(:,:,:,i) = tmp_z;
+                imout_x(:,:,:,i) = tmp_x;
+            case GRAY
+                % vl_imreadjpeg returns images in [0, 255] with class single.
+                imout_z(:,:,:,i) = repmat(rgb2gray(tmp_z/255)*255, [1 1 3]);
+                imout_x(:,:,:,i) = repmat(rgb2gray(tmp_x/255)*255, [1 1 3]);
+        endne
+
+        if opts.subMean
+            % Sanity check - mean should be in range 0-255!
+            means = [opts.stats.rgbMean_z(:); opts.stats.rgbMean_x(:)];
+            lower = 0.2 * 255;
+            upper = 0.8 * 255;
+            if ~all((lower <= means) & (means <= upper))
+                error('mean does not seem to for pixels in 0-255');
+            end
+            imout_z = bsxfun(@minus, imout_z, reshape(opts.stats.rgbMean_z, [1 1 3]));
+            imout_x = bsxfun(@minus, imout_x, reshape(opts.stats.rgbMean_x, [1 1 3]));
+        end
+        imout_z = imout_z / 255 * opts.colorRange;
+        imout_x = imout_x / 255 * opts.colorRange;
+    end
+
+    sizes_z = zeros(2, batch_size);
+    sizes_x = zeros(2, batch_size);
+    for i = 1:batch_size
+        if ~(labels(i) > 0)
+            continue
+        end
+
+        % compute bounding boxes of objects within crops x and z
+        [bbox_z, bbox_x] = get_crops(double(objects.z{i}.extent), double(objects.x{i}.extent), opts.exemplarSize, opts.instanceSize);
+        % only store h and w
+        sizes_z(:,i) = bbox_z([4 3]);
+        sizes_x(:,i) = bbox_x([4 3]);
+    end
+end
+
+
+% -----------------------------------------------------------------------------------------------------------------------
+function [z, x] = choose_pos_pair(imdb_video, rand_vid, frameRange)
+% Get positive pair with crops from same videos, centered on the object
+% -----------------------------------------------------------------------------------------------------------------------
+    valid_trackids = find(imdb_video.valid_trackids(:, rand_vid) > 1);
+    assert(~isempty(valid_trackids), 'No valid trackids for a video in the batch.');
+    rand_trackid_z = datasample(valid_trackids, 1);
+    % pick valid exemplar from the random trackid
+    rand_z = datasample(imdb_video.valid_per_trackid{rand_trackid_z, rand_vid}, 1);
+    % pick valid instance within frameRange seconds from the exemplar, excluding the exemplar itself
+    possible_x_pos = (1:numel(imdb_video.valid_per_trackid{rand_trackid_z, rand_vid}));
+    [~, rand_z_pos] = ismember(rand_z, imdb_video.valid_per_trackid{rand_trackid_z, rand_vid});
+    possible_x_pos = possible_x_pos([max(rand_z_pos-frameRange, 1):(rand_z_pos-1), (rand_z_pos+1):min(rand_z_pos+frameRange, numel(possible_x_pos))]);
+    possible_x = imdb_video.valid_per_trackid{rand_trackid_z, rand_vid}(possible_x_pos);
+    assert(~isempty(possible_x), 'No valid x for the chosen z.');
+    rand_x = datasample(possible_x, 1);
+    assert(imdb_video.objects{rand_vid}{rand_x}.valid, 'error picking rand x.');
+    z = imdb_video.objects{rand_vid}{rand_z};
+    x = imdb_video.objects{rand_vid}{rand_x};
+end
+
+
+% -----------------------------------------------------------------------------------------------------------------------
+function imo = acquire_augment(im, imageSize, rgbVariance, aug_opts)
+% Apply transformations and augmentations to original crops
+% -----------------------------------------------------------------------------------------------------------------------
+    if numel(imageSize) == 1
+        imageSize = [imageSize, imageSize];
+    end
+    if numel(aug_opts.maxTranslate) == 1
+        aug_opts.maxTranslate = [aug_opts.maxTranslate, aug_opts.maxTranslate];
+    end
+
+    imt = im;
+    if size(imt,3) == 1
+        imt = cat(3, imt, imt, imt);
+    end
+
+    w = size(imt,2) ;
+    h = size(imt,1) ;
+    cx = (w+1)/2;
+    cy = (h+1)/2;
+
+    if aug_opts.stretch
+        scale = (1+aug_opts.maxStretch*(-1+2*rand(2,1)));
+        sz = round(min(imageSize(1:2)' .* scale, [h;w]));
+    else
+        sz = imageSize;
+    end
+
+    if aug_opts.translate
+        if isempty(aug_opts.maxTranslate)
+            % Take any crop within the image.
+            dx = randi(w - sz(2) + 1, 1) ;
+            dy = randi(h - sz(1) + 1, 1) ;
+        else
+            % Take crop within maxTranslate of center.
+            mx = min(aug_opts.maxTranslate(2), floor((w-sz(2))/2));
+            my = min(aug_opts.maxTranslate(1), floor((h-sz(1))/2));
+            % Check bounds:
+            % dx = (w+1)/2 - (sz(2)-1)/2 - (w-sz(2))/2
+            %    = (w+1 - sz(2)+1 - w+sz(2))/2
+            %    = 1 + (w - sz(2) - w+sz(2))/2
+            %    = 1
+            % dx + sz(2)-1 = (w+1)/2 - (sz(2)-1)/2 + (w-sz(2))/2 + sz(2)-1
+            %              = (w+1 - sz(2)+1 + w-sz(2) + 2*sz(2)-2)/2
+            %              = (w - sz(2) + w-sz(2) + 2*sz(2))/2
+            %              = (w + w)/2
+            %              = w
+            dx = cx - (sz(2)-1)/2 + randi([-mx, mx], 1);
+            dy = cy - (sz(1)-1)/2 + randi([-my, my], 1);
+        end
+    else
+        % Take crop at center.
+        dx = cx - (sz(2)-1)/2;
+        dy = cy - (sz(1)-1)/2;
+    end
+    sx = round(linspace(dx, dx+sz(2)-1, imageSize(2))) ;
+    sy = round(linspace(dy, dy+sz(1)-1, imageSize(1))) ;
+
+    % flip = rand > 0.5 ;
+    % if flip, sx = fliplr(sx) ; end
+
+    if ~aug_opts.color
+        imo = imt(sy,sx,:);
+    else
+        offset = reshape(rgbVariance * randn(3,1), 1,1,3);
+        imo = bsxfun(@minus, imt(sy,sx,:), offset);
+    end
+end
+
+
+% -----------------------------------------------------------------------------------------------------------------------
+function [bbox_z, bbox_x] = get_crops(object_z_extent, object_x_extent, size_z, size_x)
+% Compute objects bbox within crops
+% bboxes are returned as [xmin, ymin, width, height]
+% -----------------------------------------------------------------------------------------------------------------------
+
+    [w_z, h_z] = deal(object_z_extent(3), object_z_extent(4));
+    context_amount = 0.5;
+    wc_z = w_z + context_amount*(w_z+h_z);
+    hc_z = h_z + context_amount*(w_z+h_z);
+    s_z = sqrt(wc_z*hc_z);
+    scale_z = size_z / s_z;
+    %% get instance sample
+    [w_x, h_x] = deal(object_x_extent(3), object_x_extent(4));
+    d_search = (size_x - size_z)/2;
+    pad = d_search/scale_z;
+    s_x = s_z + 2*pad;
+    scale_x = size_x / s_x;
+    % Size of true object within the crops
+    ws_z = w_z * scale_z;
+    hs_z = h_z * scale_z;
+    ws_x = w_x * scale_x;
+    hs_x = h_x * scale_x;
+    bbox_z = [(size_z-ws_z)/2, (size_z-hs_z)/2, ws_z, hs_z];
+    bbox_x = [(size_x-ws_x)/2, (size_x-hs_x)/2, ws_x, hs_x];
+end
