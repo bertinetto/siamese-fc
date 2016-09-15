e58a022c833b43447b374b26239ac0ab8c363285
Luca Bertinetto <luca.bertinetto@gmail.com>
2016-09-15 14:58:13 +0100

    minor fix get obj extent

--- training/vid_get_random_batch.m
+++ training/vid_get_random_batch.m
@@ -139,7 +139,7 @@ function [imout_z, imout_x, labels, sizes_z, sizes_x] = vid_get_random_batch(imd
         end
 
         % compute bounding boxes of objects within crops x and z
-        [bbox_z, bbox_x] = get_crops(double(objects.z{i}.extent), double(objects.x{i}.extent), opts.exemplarSize, opts.instanceSize);
+        [bbox_z, bbox_x] = get_objects_extent(double(objects.z{i}.extent), double(objects.x{i}.extent), opts.exemplarSize, opts.instanceSize);
         % only store h and w
         sizes_z(:,i) = bbox_z([4 3]);
         sizes_x(:,i) = bbox_x([4 3]);
@@ -237,28 +237,35 @@ function imo = acquire_augment(im, imageSize, rgbVariance, aug_opts)
 end
 
 % -----------------------------------------------------------------------------------------------------------------------
-function [bbox_z, bbox_x] = get_crops(object_z_extent, object_x_extent, size_z, size_x)
+function [bbox_z, bbox_x] = get_objects_extent(object_z_extent, object_x_extent, size_z, size_x)
 % Compute objects bbox within crops
 % bboxes are returned as [xmin, ymin, width, height]
 % -----------------------------------------------------------------------------------------------------------------------
-
-    [w_z, h_z] = deal(object_z_extent(3), object_z_extent(4));
+    % TODO: this should passed from experiment as default
     context_amount = 0.5;
-    wc_z = w_z + context_amount*(w_z+h_z);
+    
+    % getting in-crop object extent for Z
+    [w_z, h_z] = deal(object_z_extent(3), object_z_extent(4));
+    wc_z = w_z + context_amount*(w_z+h_z);   
     hc_z = h_z + context_amount*(w_z+h_z);
     s_z = sqrt(wc_z*hc_z);
-    scale_z = size_z / s_z;
-    %% get instance sample
-    [w_x, h_x] = deal(object_x_extent(3), object_x_extent(4));
-    d_search = (size_x - size_z)/2;
-    pad = d_search/scale_z;
-    s_x = s_z + 2*pad;
-    scale_x = size_x / s_x;
-    % Size of true object within the crops
+    scale_z = size_z / s_z;   
     ws_z = w_z * scale_z;
     hs_z = h_z * scale_z;
+    bbox_z = [(size_z-ws_z)/2, (size_z-hs_z)/2, ws_z, hs_z];
+    
+    % getting in-crop object extent for X    
+    [w_x, h_x] = deal(object_x_extent(3), object_x_extent(4));
+    wc_x = w_x + context_amount*(w_x+h_x);   
+    hc_x = h_x + context_amount*(w_x+h_x);
+    s_xz = sqrt(wc_x*hc_x);
+    scale_xz = size_z / s_xz;
+    
+    d_search = (size_x - size_z)/2;
+    pad = d_search/scale_xz;
+    s_x = s_xz + 2*pad;
+    scale_x = size_x / s_x;
     ws_x = w_x * scale_x;
     hs_x = h_x * scale_x;
-    bbox_z = [(size_z-ws_z)/2, (size_z-hs_z)/2, ws_z, hs_z];
     bbox_x = [(size_x-ws_x)/2, (size_x-hs_x)/2, ws_x, hs_x];
 end
